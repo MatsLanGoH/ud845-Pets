d569ffbf2f6ca86de98227b1f1f986009456d353
Matthias Lambrecht <mats.lan.pod@googlemail.com>
2017-07-02 00:14:55 +0900

    Reflect changes from 2-10

---
+++ app/src/main/java/com/example/android/pets/data/PetDbHelper.java
@@ -0,0 +1,30 @@
+package com.example.android.pets.data;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by sirfelius on 2017/07/01.
+ */
+
+public class PetDbHelper extends SQLiteOpenHelper {
+
+    // TODO: Create constants for database name and database version
+
+    // TODO: Create a constructor
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        // TODO: Implement the onCreate() method
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // TODO: Implement the onUpgrade() method
+    }
+
+
+
+
+}
+
